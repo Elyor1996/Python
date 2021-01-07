727622fce6e1146dcf61dd956d7b3e4793897b87
Elyor1996 <elyor@bnpz.uz>
2021-01-07 22:13:53 +0500

    kechagi

---
+++ 2-dars.py
@@ -0,0 +1,18 @@
+text_1 = 'python dasturlash tili'
+son = 500
+comments = '10'
+print(type(text_1))
+print(type(son))
+print(type(comments))
+
+
+a = 5
+b = 6
+c = a + b
+print('Result:', c)
+
+
+a = input('enter first number: ')
+b = input('ikkinchi sonni kiriting: ')
+c = int(a) + int(b)
+print(c)
---
+++ 3_dars.py
@@ -0,0 +1,11 @@
+# narx = 3500
+# soni = int(input('nechta non olmoqchisiz: '))
+# natija = soni * narx
+# print('natija:', natija)
+
+a = int(input('enter 1 st number: '))
+b = int(input('enter 2 nd number: '))
+c = a
+a = b
+b = c
+print(a,b)
