01fcec09ae4167432aebf43f2bac713c50369337
Elyor1996 <elyor@bnpz.uz>
2021-01-10 21:43:45 +0500

    4-dars

--- 4_dars.py
+++ 4_dars.py
@@ -1,4 +1,35 @@
-print('operaton +', 25 + 5)
-print('operaton -', 25 - 5)
-print('operaton *', 25 * 5)
-print('operaton /', 25 / 5)
+# print('operaton +', 25 + 5)
+# print('operaton -', 25 - 5)
+# print('operaton *', 25 * 5)
+# print('operaton /', 25 / 5)
+#
+# print('operator %', 18%4) # % - qoldiqni ifodalovchi operator
+# print('operator //', 18//4) # // - butun qismini ko'rsatadi
+# print('operator **', 3**3) # ** - sonni kvadratga ko'tarish
+# a = 25
+# a +=5
+# print(a)
+
+
+#-#-#-#-# 1kg limon a so'm b tiyin.
+
+# a = int(input("so'm: " ))
+# b = int(input("tiyin: "))
+# n = int(input("Kg: "))
+# c = (a*100+b)*n
+# print("so`m: ", c//100 )
+# print("tiyin: ", c%100 )
+
+
+
+#-#-#-#-# istalgan kiritilgan sonning oxirgi raqamini topib berivchi dastur
+
+# a = int(input("Istalgan butun sonni kiriting: "))
+# print("bu sonning oxirgi raqami: ", a%10)
+
+
+#-#-#-#-# istalgan kiritilgan butun sonning ikkinchi raqamini topuvchi dastur
+
+a = int(input("istalgan butun sonni kiriting: "))
+c = a%100
+print("bu sonning ikkinchi raqami: ", c//10)
