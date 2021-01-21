b3192f0c2f6a1daedcbf1d8df85eb2d6f7ba5747
Elyor1996 <elyor@bnpz.uz>
2021-01-21 21:42:10 +0500

    saved

--- 7-dars.py
+++ 7-dars.py
@@ -2,15 +2,15 @@
 # if ---- agar
 # elif ---- aks holda agar
 # else ----- aks holda
-til = input('tilni tanlang: uz/en ?')
-
-if til == 'uz':
-    print('Assalomu alaykum')
-elif til == 'en':
-    print('Hello!')
-else:
-    print('xato tanlandi.')
-
+# til = input('tilni tanlang: uz/en ?')
+# -----------------------------------
+# if til == 'uz':
+#     print('Assalomu alaykum')
+# elif til == 'en':
+#     print('Hello!')
+# else:
+#     print('xato tanlandi.')
+# -----------------------------------
 # Equals: a == b
 # Not Equals: a != b
 # Less than: a < b
@@ -25,10 +25,30 @@ from random import randint
 
 a = randint(1, 500)
 b = randint(1, 500)
+c = input('{} + {} = '.format(a, b))
+k = 0
+m = 0
+while c.isdigit():
+   
+    if int(c) == (a + b):
+        print('To`g`ri')
+        k+=1
+    else:
+        print('Xato!')
+        m+=1
+    a = randint(1, 500)
+    b = randint(1, 500)
+    c = input('{} + {} = '.format(a, b))
 
-c = int(input('{} + {} = '.format(a, b)))
+print('Dastur tugadi!')
+print('To`g`ri javoblar: {}'.format(k))
+print('Xato javoblar: {}'.format(m))
 
-if c == (a + b):
-    print('To`g`ri')
-else:
-    print('Xato!')
+#-------------------------------------------
+#Masala
+# a = int(input('yilni kiriting: '))
+# c = a%4
+# if c == 0:
+#     print('bu kabisa yili:') 
+# else:
+#     print('bu kabbisya yili emas:')
